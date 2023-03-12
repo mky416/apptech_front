32cf33bfe352dac65c7d6862193cfc0ef367a095
keunyoungma <mky416@gmail.com>
2023-03-12 11:23:23 +0900

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button } from 'react-bootstrap';
 
 function App() {
   return (
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Button>aaa</Button>
       </header>
     </div>
   );
